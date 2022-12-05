c630fbc390fad0da37a4e9cb591dde2386ae2f59
Gcav66 <gustafcavanaugh@gmail.com>
2022-12-05 05:11:55 -0500

    create ml artifact dir

--- dagster_mlflow_project/assets/__init__.py
+++ dagster_mlflow_project/assets/__init__.py
@@ -82,14 +82,14 @@ def run():
         logging.debug(f'Tracking URL: {tracking_url_type_store}')
 
         # Model registry does not work with file store
-        #if tracking_url_type_store != "file":
+        if tracking_url_type_store != "file":
 
             # Register the model
             # There are other ways to use the Model Registry, which depends on the use case,
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            #mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
-        #else:
-            #mlflow.sklearn.log_model(lr, "model")
+            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+        else:
+            mlflow.sklearn.log_model(lr, "sk_models")
     print("Model training complete")
     return True
